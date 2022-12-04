fb0c1749461a9c99c8cbc8c72dd632059634caf9
NehaK4 <nehapumca@gmail.com>
2022-12-05 02:56:45 +0530

    Refactoring code

--- src/main/com/javac/plugin/InferredT.java
+++ src/main/com/javac/plugin/InferredT.java
@@ -5,6 +5,6 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)   
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER, ElementType.TYPE}) 
-public @interface GCheck {
+public @interface InferredT {
   
 }
